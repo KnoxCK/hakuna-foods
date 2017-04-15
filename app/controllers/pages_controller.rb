3c538fa9bff5feb9d090a8fc1d44d5f78866241b
Clarissa Knox <knox.ck@gmail.com>
2017-04-15 12:42:32 +0100

    navbars & routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :mealplans, :ourfood, :order, :about ]
 
   def home
+  end
+
+  def mealplans
+  end
+
+  def ourfood
+  end
+
+  def order
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'mealplans', to: 'pages#mealplans'
+  get 'ourfood', to: 'pages#ourfood'
+  get 'order', to: 'pages#order'
+  get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
