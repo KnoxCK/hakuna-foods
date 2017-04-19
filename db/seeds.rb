c70bc87a35a816e3735b8204e0459eb15dbbcb2c
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-19 12:33:06 +0100

    products, categories and fake user seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@ User.destroy_all
 
 User.create(email: 'guv@test.com', password: '123456')
 
-Category.create!(name: 'Meal Plans')
+Category.create!(name: 'Meal Plan')
 Category.create!(name: 'Extras')
 
-Product.create!(name: 'Maintain', sku: "maintain-monthly-plan", daily_price: 39, category: 1)
-Product.create!(name: 'Train', sku: "train-monthly-plan", daily_price: 41, category: 1)
-Product.create!(name: 'Trim', sku: "trim-monthly-plan", daily_price: 36, category: 1)
-Product.create!(name: 'Postnatal', sku: "postnatal-monthly-plan", daily_price: 39, category: 1)
-Product.create!(name: 'Vegetarian', sku: "vegetarian-monthly-plan", daily_price: 39, category: 1)
-Product.create!(name: 'Bespoke', sku: "bespoke-monthly-plan", daily_price: 0, category: 1)
+Product.create!(name: 'Maintain', sku: "maintain-monthly-plan", daily_price: 39, category_id: Category.where(name: 'Meal Plan').first.id)
+Product.create!(name: 'Train', sku: "train-monthly-plan", daily_price: 41, category_id: Category.where(name: 'Meal Plan').first.id)
+Product.create!(name: 'Trim', sku: "trim-monthly-plan", daily_price: 36, category_id: Category.where(name: 'Meal Plan').first.id)
+Product.create!(name: 'Postnatal', sku: "postnatal-monthly-plan", daily_price: 39, category_id: Category.where(name: 'Meal Plan').first.id)
+Product.create!(name: 'Vegetarian', sku: "vegetarian-monthly-plan", daily_price: 39, category_id: Category.where(name: 'Meal Plan').first.id)
+Product.create!(name: 'Bespoke', sku: "bespoke-monthly-plan", daily_price: 0, category_id: Category.where(name: 'Meal Plan').first.id)
 
-Product.create!(name: 'Kombucha', sku: "kombucha-extra", daily_price: 2.5, category: 2)
-Product.create!(name: 'Cold Press Coffee', sku: "coffee-extra", daily_price: 2.5, category: 2)
-Product.create!(name: 'Sweet Treat', sku: "sweet-extra", daily_price: 2.5, category: 2)
+Product.create!(name: 'Kombucha', sku: "kombucha-extra", daily_price: 2.5, category_id: Category.where(name: 'Extras').first.id)
+Product.create!(name: 'Cold Press Coffee', sku: "coffee-extra", daily_price: 2.5, category_id: Category.where(name: 'Extras').first.id)
+Product.create!(name: 'Sweet Treat', sku: "sweet-extra", daily_price: 2.5, category_id: Category.where(name: 'Extras').first.id)
