3f7e4f1277053df444743b580bd5d87738e220b0
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-19 12:35:05 +0100

    added description to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418155345) do
+ActiveRecord::Schema.define(version: 20170419113352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170418155345) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
