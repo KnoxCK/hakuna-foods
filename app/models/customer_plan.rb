d2684b8f909ec2aa12c1810126b27198ad1cde5d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-20 13:50:46 +0100

    associations change

--- app/models/customer_plan.rb
+++ app/models/customer_plan.rb
@@ -5,6 +5,4 @@ class CustomerPlan < ApplicationRecord
   has_many :products, through: :extra_items
   has_one :meal_plan, :class_name => 'Product', :foreign_key => 'meal_plan_id'
 
-  def days_per_week
-  end
 end
--- app/models/extra_item.rb
+++ app/models/extra_item.rb
@@ -1,5 +1,5 @@
 class ExtraItem < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, through: :customer_plan
   has_one :product
   belongs_to :customer_plan
 end
