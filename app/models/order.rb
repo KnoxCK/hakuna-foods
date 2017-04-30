70c24a10a1c51df0fcaf25c19a85afaf46e53211
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-30 21:52:50 +0100

    order model change

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :customer, through: :meal_plan
-  has_one :meal_plan
+  belongs_to :customer, through: :customer_plan
+  has_one :customer_plan
 
   monetize :price_pennies
 
