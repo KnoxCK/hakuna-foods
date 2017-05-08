2cc65cf1cecae06db5a19427667706b40527aa52
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-08 21:29:59 +0100

    orders payment removed

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -39,10 +39,16 @@ class PaymentsController < ApplicationController
     end
 
     @customer.update(stripe_customer_id: customer.id)
-    @order.update(payment: charge.to_json, state: 'Paid')
+    @order.update(state: 'Paid')
 
     redirect_to customer_customer_plan_order_path(@customer, @customer_plan, @order)
 
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_order_payment_path(@order)
+  end
+
+  protect_from_forgery
 
   end
 
