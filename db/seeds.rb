832756bd05b402b74528286f71039964d705cb52
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-20 10:50:39 +0100

    new fields in meal plan seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ User.destroy_all
 
 User.create!(email: "guv@test.com", password: "123456", admin: true)
 
-MealPlan.create!(name: 'Maintain', daily_price: 39)
-MealPlan.create!(name: 'Train', daily_price: 41)
-MealPlan.create!(name: 'Trim', daily_price: 36)
-MealPlan.create!(name: 'Postnatal', daily_price: 39)
-MealPlan.create!(name: 'Vegetarian', daily_price: 39)
-MealPlan.create!(name: 'Bespoke', daily_price: 0)
+MealPlan.create!(name: 'Maintain', daily_price: 39, description: '', detailed_description: '')
+MealPlan.create!(name: 'Train', daily_price: 41, description: '', detailed_description: '')
+MealPlan.create!(name: 'Trim', daily_price: 36, description: '', detailed_description: '')
+MealPlan.create!(name: 'Postnatal', daily_price: 39, description: '', detailed_description: '')
+MealPlan.create!(name: 'Vegetarian', daily_price: 39, description: '', detailed_description: '')
+MealPlan.create!(name: 'Bespoke', daily_price: 0, description: '', detailed_description: '')
 
 Extra.create!(name: 'Kombucha', unit_price: 2.5)
 Extra.create!(name: 'Cold Press Coffee',  unit_price: 2.5)
