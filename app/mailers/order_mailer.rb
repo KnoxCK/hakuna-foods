20f1b44d0da981deae771fa2e45323eb4fdbb423
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-30 17:51:16 +0100

    order confirmation mailer done

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,7 @@ class OrderMailer < ApplicationMailer
   def order_confirmation(customer)
     @customer = customer
 
-    mail (to: @customer.email
+    mail(to: @customer.email,
           subject: "Your Hakuna Foods Order Confirmation")
   end
 end
