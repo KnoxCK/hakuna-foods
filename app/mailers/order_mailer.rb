6907c58d80d8c34c73a721c48223cc3e3c0a0c44
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-27 21:13:54 +0100

    bespoke request mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,4 +5,11 @@ class OrderMailer < ApplicationMailer
     mail(to: @customer.email,
           subject: "Your Hakuna Foods Order Confirmation")
   end
+
+  def bespoke_request(customer)
+    @customer = customer
+
+    mail(to: info@hakunafoods.co.uk,
+          subject: "You have a request for a bespoke meal plan")
+  end
 end
