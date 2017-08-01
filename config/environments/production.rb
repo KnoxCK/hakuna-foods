bdb93266bdfac6ca2b44d2a8844c3c2591a3dcc3
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-01 13:35:24 +0100

    postmark config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: "https://hakuna-foods.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
