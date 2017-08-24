076b4529ca0caa56046cb98316ac118a18b3ac14
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-24 10:43:24 +0100

    mailer added

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,10 +6,17 @@ class OrderMailer < ApplicationMailer
           subject: "Your Hakuna Foods Order Confirmation")
   end
 
+  def order_received(customer)
+    @customer = customer
+
+    mail(to: 'info@hakunafoods.co.uk',
+          subject: "You have recieved a new order!")
+  end
+
   def bespoke_request(customer)
     @customer = customer
 
-    mail(to: "govinder.marwaha@gmail.com",
+    mail(to: "info@gmail.com",
           subject: "You have been sent a request for a bespoke meal plan")
   end
 end
