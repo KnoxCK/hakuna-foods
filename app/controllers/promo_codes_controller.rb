5ded21ea2ea3e46a51c691dcd5a04aed4a06e4b7
Govinder Marwaha <govinder.marwaha@gmail.com>
2018-01-01 15:00:30 +0000

    message styling

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -4,6 +4,6 @@ class PromoCodesController < ApplicationController
   def verify
     PromoCode.verify(params[:customer_plan_id], params[:code])
     @customer_plan = CustomerPlan.find(params[:customer_plan_id])
-    @message = @customer_plan.promo_verified? ? "Verified" : "Code invalid"
+    @message = @customer_plan.promo_verified? ? "Verified - your £#{@customer_plan.promo_code.discount} discount has been applied!" : "Code invalid"
   end
 end
