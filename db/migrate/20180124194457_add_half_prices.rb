70e3dbee66a72154c86ded663bd9ea7c9c0d95f0
Govinder Marwaha <govinder.marwaha@gmail.com>
2018-01-24 19:50:56 +0000

    half price migration

--- db/migrate/20180124194457_add_half_prices.rb
+++ db/migrate/20180124194457_add_half_prices.rb
@@ -3,7 +3,7 @@ class AddHalfPrices < ActiveRecord::Migration[5.0]
     MealPlan.where(name: 'Maintain').update(half_daily_price: 21)
     MealPlan.where(name: 'Train').update(half_daily_price: 22)
     MealPlan.where(name: 'Trim').update(half_daily_price: 20)
-    MealPlan.where(name: 'Postnatal').update(half_daily_price: 21
-    MealPlan.where(name: 'Plant Based').update(half_daily_price: 19
+    MealPlan.where(name: 'Postnatal').update(half_daily_price: 21)
+    MealPlan.where(name: 'Plant Based').update(half_daily_price: 19)
   end
 end
