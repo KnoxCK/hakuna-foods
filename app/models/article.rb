fb266999a9e8c25856ad989f61256de45f8d2a31
Arthur Littmann <arthur@lewagon.org>
2019-05-01 14:46:41 +0100

    blog with label for published

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ApplicationRecord
   mount_uploader :cover_image, PhotoUploader
+
+  validates :title, :content, :description, :date, presence: true
+
+  def slug
+    title.parameterize
+  end
 end
+
+# t.string "title"
+    # t.string "content"
+    # t.boolean "published"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+    # t.string "cover_image"
+    # t.text "description"
+    # t.date "date"
